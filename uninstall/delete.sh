0553d4978c88473ec7dc963de10b8b53b118557a
Mariusz Jasinski <Mariusz.Jasinski@asseco.pl>
2020-03-23 16:34:06 +0100

    uninstall

---
+++ uninstall/delete.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Reading configuration
+. ../configure.env
+
+# active project variable
+project_name="${run_project_name}${environment}"
+project_build=`echo "${build_project_name}${environment}"`
+
+oc delete -f kong-account-deletes.yaml -n ${project_name}
+oc delete project ${project_name}
+oc delete -f kong-cluster-deletes.yaml
+
+oc delete bc ${build_name} -n ${project_build}
